a9ee6a655b3b81cf1ff43b11e40f2a313539d9bc
Stephen Carico <stephen_carico@hotmail.com>
2015-09-11 02:54:06 +0000

    Added notice and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
   attr_accessor :stripe_card_token
-  
   def save_with_payment
-    if valid? 
+    if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
